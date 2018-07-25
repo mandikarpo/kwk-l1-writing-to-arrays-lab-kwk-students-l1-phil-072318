1cf30c2dc5398759be46b2666ad5bbcd7ce290b7
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-25 15:13:56 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,9 +6,6 @@ rainbow_colors = [
 
 def change_rainbow_colors
   color = ["red", "light_red", "light_yellow"]
-  color [0] == "red"
-  color [1] == "light_red"
-  color [2] == "light_yellow"
   puts color
 end
 change_rainbow_colors
